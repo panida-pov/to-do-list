ff2a3defe49d10c0cef137492b3ad325180fd314
nitt-sukhums <nitt.sukhums@gmail.com>
2024-02-19 00:40:19 +0900

    setup docker files and migration on typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbDataSource } from './data.source';
 
 @Module({
-  imports: [TaskModule],
+  imports: [TypeOrmModule.forRoot(dbDataSource), TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
