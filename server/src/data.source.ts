19c2b5d7834a71b703241aab7d1392093e0eeca6
panida-pov <panida.pov@gmail.com>
2024-02-19 20:16:38 +0900

    add entities to datasource

--- server/src/data.source.ts
+++ server/src/data.source.ts
@@ -2,11 +2,12 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dbDataSource: DataSourceOptions = {
   type: 'mysql',
-  host: 'to-do-db',
-  port: 3306,
+  host: 'localhost', //to-do-db
+  port: 23306, //3306
   username: 'root',
   password: 'root',
   database: 'todo',
+  entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
   migrationsRun: false,
   migrations: [__dirname + '/migration/*.ts'],
   migrationsTableName: 'migration',
