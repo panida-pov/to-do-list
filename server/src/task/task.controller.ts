1d45da21463d2182c1e6dca7b16c10379deba90a
panida-pov <panida.pov@gmail.com>
2024-02-19 20:19:05 +0900

    create basic crud for tasks

--- server/src/task/task.controller.ts
+++ server/src/task/task.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param, Delete} from '@nestjs/common';
 import { TaskService } from './task.service';
+import { TaskEntity } from './task.entity';
 
-@Controller('task')
+@Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Get()
-  getTask(): string {
-    return this.taskService.getTask();
+  async getTask(): Promise<TaskEntity[]> {
+    return await this.taskService.findAll();
+  }
+
+  @Post()
+  async createTask(@Body() newTask: TaskEntity): Promise<TaskEntity> {
+    return await this.taskService.create(newTask);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() updateTask: TaskEntity): Promise<TaskEntity> {
+    return await this.taskService.update(id, updateTask);
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') id: number) {
+    return await this.taskService.delete(id);
   }
 }
--- server/src/task/task.service.ts
+++ server/src/task/task.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, DeleteResult } from 'typeorm';
+import { TaskEntity } from './task.entity';
 
 @Injectable()
 export class TaskService {
-  getTask(): string {
-    return 'This is your task!';
+  constructor(
+    @InjectRepository(TaskEntity)
+    private readonly tasksRepository: Repository<TaskEntity>,
+  ) {}
+
+  async findAll(): Promise<TaskEntity[]> {
+    return this.tasksRepository.find();
+  }
+
+  async create(newTask: TaskEntity): Promise<TaskEntity> {
+    return await this.tasksRepository.save(newTask);
+  }
+
+  async update(id: number, updateTask: TaskEntity): Promise<TaskEntity> {
+    let toUpdate = await this.tasksRepository.findOneBy({ id: id });
+    let updated = Object.assign(toUpdate, updateTask);
+    return await this.tasksRepository.save(updated);
+  }
+ 
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.tasksRepository.delete({ id: id });
   }
 }
