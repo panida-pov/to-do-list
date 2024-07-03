3addd482d0a7d30d0cb89f06d723832be9b9fdc0
panida-pov <panida.pov@gmail.com>
2024-07-03 12:32:42 +0900

    add env variables for deploying

--- server/src/data.source.ts
+++ server/src/data.source.ts
@@ -3,11 +3,11 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dbDataSource: DataSourceOptions = {
   type: 'mysql',
-  host: process.env.HOST || 'to-do-db',
-  port: parseInt(process.env.PORT, 10) || 3306,
-  username: 'root',
-  password: 'root',
-  database: 'todo',
+  host: process.env.DB_HOST || 'to-do-db',
+  port: parseInt(process.env.DB_PORT, 10) || 3306,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DBNAME || 'todo',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrationsRun: true,
   migrations: [__dirname + '/migration/*{.ts,.js}'],
